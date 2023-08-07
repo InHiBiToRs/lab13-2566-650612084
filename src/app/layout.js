6c3d2639bb1ccb70dd56001ba08e85b1d29148b3
InHiBiToRs <Tanakrit64566@gmail.com>
2023-08-07 15:34:21 +0700

    lab13_complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Tanakrit Boonyoung"
+            studentId="650612084"
           />
         </div>
       </body>
